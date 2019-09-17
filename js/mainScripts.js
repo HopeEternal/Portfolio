6bec696fa34a9728c37798ceac644804037505aa
Risa <42893634+HopeEternal@users.noreply.github.com>
2019-09-17 09:19:06 -0600

    Second Upload

---
+++ js/mainScripts.js
@@ -0,0 +1,8 @@
+//Footer Copyright Year Autogenerate
+document.getElementById('curYear').textContent = new Date().getFullYear();
+
+//Years of Experience Calculate and Autogenerate
+document.getElementById('yrsExp').textContent = new Date().getFullYear() - 2011;
+
+			
+			
